29ee62c09fb2de13214bcc9bbba29e9cf6e7b73c
BuildTools <unconfigured@null.spigotmc.org>
2020-09-21 21:09:34 -0400

    i

--- todo_web.py
+++ todo_web.py
@@ -37,7 +37,7 @@ def post_new_item():
 
 
 @get('/delete_item/<id>')
-def get_delete_item():
+def get_delete_item(id):
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
     cursor.execute("delete from todo where id=?", (id,))
